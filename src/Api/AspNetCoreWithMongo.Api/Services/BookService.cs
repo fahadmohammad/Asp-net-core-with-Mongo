e3d8db238978fce61e4536d99be168c56278d56c
Fahad <fahadmo0hammad@gmail.com>
2020-06-25 18:09:19 +0600

    service added.

---
+++ src/Api/AspNetCoreWithMongo.Api/Services/BookService.cs
@@ -0,0 +1,38 @@
+﻿using AspNetCoreWithMongo.Api.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWithMongo.Api.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+        public BookService(IDbSettings dbSettings)
+        {
+            var client = new MongoClient(dbSettings.ConnectionString);
+            var database = client.GetDatabase(dbSettings.DatabaseName);
+
+            _books = database.GetCollection<Book>(dbSettings.CollectionName);
+        }
+
+        public List<Book> Get() =>
+            _books.Find(book => true).ToList();
+        public Book Get(string id) =>
+            _books.Find(book => book.Id == id).FirstOrDefault();
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+        public void Update(string id, Book bookToUpdate) =>
+            _books.ReplaceOne(book => book.Id == id, bookToUpdate);
+        public void Remove(Book bookToRemove) =>
+            _books.DeleteOne(book => book.Id == bookToRemove.Id);
+        public void Remove(string id) =>
+            _books.DeleteOne(book => book.Id == id);
+
+    }
+}
---
+++ src/Api/AspNetCoreWithMongo.Api/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreWithMongo.Api.Models;
+using AspNetCoreWithMongo.Api.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace AspNetCoreWithMongo.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<BookStoreDbSettings>(Configuration.GetSection(nameof(BookStoreDbSettings)));
+            services.AddSingleton<IDbSettings>(sp => sp.GetRequiredService<IOptions<BookStoreDbSettings>>().Value);
+            services.AddSingleton<BookService>();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
